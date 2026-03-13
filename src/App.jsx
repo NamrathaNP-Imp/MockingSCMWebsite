3b2e69a61afca6d370fbeb031c48c6b5150bd462
NamrathaNP <namratha.petlar@impelsys.com>
2026-03-13 17:39:50 +0530

    login, register callbacks

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Dashboard from './Dashboard';
 
 const App = () => {
   const [loading, setloading] = useState(true);
-  const [isloggedin, setLoggedin]= useState(false);
+  const [isloggedin, setLoggedin] = useState(false);
   const [userName, setUserName] = useState('');
-  const [showToast, setToastData] = useState ({
+  const [showToast, setToastData] = useState({
     show: false,
     message: '',
   });
@@ -66,132 +66,133 @@ const App = () => {
       }
     }, pollInterval);
   }
-
-
-  useEffect(() => {
+  const onloadCalls = () => {
     waitForSDKAndRenderForm();
     const interval = setInterval(() => {
-    const logindata = window.IIRISPassport.irisLoginCallback;
+      const logindata = window.IIRISPassport.irisLoginCallback;
 
-    if (logindata?.success) {
-      setToastData({
-        show: true,
-        message: logindata.data?.message||logindata.error?.message || "User logged in Successfully",
-      });
-      resetToast();
-      console.log("Detected login response", logindata);
-      setLoggedin(true);
-      setUserName(logindata.data?.user || '')
+      if (logindata?.success) {
+        setToastData({
+          show: true,
+          message: logindata.data?.message || logindata.error?.message || "User logged in Successfully",
+        });
+        resetToast();
+        console.log("Detected login response", logindata);
+        setLoggedin(true);
+        setUserName(logindata.data?.user || '')
+        clearInterval(interval);
+      }
+    }, 700);
+    const intervalR = setInterval(() => {
+      const registerData = window.IIRISPassport.irisRegisterCallback
+
+      if (registerData?.success) {
+        setToastData({
+          show: true,
+          message: registerData.data?.message || registerData.error?.message || "User registered Successfully",
+        });
+        resetToast();
+        clearInterval(intervalR);
+      }
+    }, 500);
+  }
+
+  useEffect(() => {
+    onloadCalls();
+
+    return () => {
       clearInterval(interval);
-    }
-  }, 700);
-   const intervalR = setInterval(() => {
-    const registerData = window.IIRISPassport.irisRegisterCallback
-    
-    if(registerData?.success){
-      setToastData({
-        show: true,
-        message: registerData.data?.message||registerData.error?.message || "User registered Successfully" ,
-      });
-      resetToast();
       clearInterval(intervalR);
     }
-  }, 500);
-
-
-  return () => {
-    clearInterval(interval);
-    clearInterval(intervalR);
-  }
   }, []);
 
-  const handleBack = () =>{
+  const handleBack = () => {
     setLoggedin(false);
   }
   useEffect(() => {
-  if (isloggedin == false && userName != '') {
-    waitForSDKAndRenderForm();
-  }
-}, [isloggedin]);
-  
+    if (isloggedin == false && userName != '') {
+      onloadCalls();
+    }
+  }, [isloggedin]);
+
 
   return (
     <div className="container">
       {isloggedin == true ? <>
-        <Dashboard user={userName} onBack={handleBack}/>
-      </>: 
-      <>
-      <div className="banner">
-        <h2>Nation's Restaurant News</h2>
-        <p>Webinar</p>
-      </div>
-      <div className="layout">
-
-        {/* LEFT COLUMN */}
-        <div className="left">
-
-          <h1 className="title">
-            Seasonal Success with Jamie Starner: Hiring Summer Staff While Staying Compliant
-          </h1>
-
-          <div className="meta">
-            <span>📅 Oct 28, 2025</span>
-            <span>⏰ 3:00 PM EDT</span>
-            <span>⏱ Duration: 2 Hr</span>
+        <Dashboard user={userName} onBack={handleBack} />
+      </> :
+        <>
+          <div className="banner">
+            <h2>Nation's Restaurant News</h2>
+            <p>Webinar</p>
           </div>
+          <div className="layout">
 
-          <p className="desc">
-            Hiring for the summer rush? Whether you're bringing on your first
-            batch of hourly workers or scaling up for your busiest season,
-            it's critical to stay compliant from day one.
-          </p>
+            {/* LEFT COLUMN */}
+            <div className="left">
 
-          <h3>Learning Objectives:</h3>
-          <ul>
-            <li>Understand key employment forms for new hires</li>
-            <li>Track employee hours and ACA compliance</li>
-            <li>Build a compliance-friendly onboarding process</li>
-          </ul>
+              <h1 className="title">
+                Seasonal Success with Jamie Starner: Hiring Summer Staff While Staying Compliant
+              </h1>
 
-          <h3>Speakers</h3>
+              <div className="meta">
+                <span>📅 Oct 28, 2025</span>
+                <span>⏰ 3:00 PM EDT</span>
+                <span>⏱ Duration: 2 Hr</span>
+              </div>
 
-          <div className="speakers">
-            <div className="speaker">
-              <img src="https://i.pravatar.cc/100?img=1" />
-              <p>Jamie Starner</p>
+              <p className="desc">
+                Hiring for the summer rush? Whether you're bringing on your first
+                batch of hourly workers or scaling up for your busiest season,
+                it's critical to stay compliant from day one.
+              </p>
+
+              <h3>Learning Objectives:</h3>
+              <ul>
+                <li>Understand key employment forms for new hires</li>
+                <li>Track employee hours and ACA compliance</li>
+                <li>Build a compliance-friendly onboarding process</li>
+              </ul>
+
+              <h3>Speakers</h3>
+
+              <div className="speakers">
+                <div className="speaker">
+                  <img src="https://i.pravatar.cc/100?img=1" />
+                  <p>Jamie Starner</p>
+                </div>
+
+                <div className="speaker">
+                  <img src="https://i.pravatar.cc/100?img=2" />
+                  <p>Emily Hancock</p>
+                </div>
+
+                <div className="speaker">
+                  <img src="https://i.pravatar.cc/100?img=3" />
+                  <p>Leigh Anne</p>
+                </div>
+              </div>
             </div>
 
-            <div className="speaker">
-              <img src="https://i.pravatar.cc/100?img=2" />
-              <p>Emily Hancock</p>
+            {/* RIGHT COLUMN */}
+            <div className="right">
+              {loading == true &&
+                <div className='rightLoader'>
+                  <CircularProgress />
+                </div>
+              }
+              <div id="auth-container">
+              </div>
             </div>
 
-            <div className="speaker">
-              <img src="https://i.pravatar.cc/100?img=3" />
-              <p>Leigh Anne</p>
-            </div>
           </div>
-        </div>
-
-        {/* RIGHT COLUMN */}
-        <div className="right">
-          {loading == true &&
-          <div className='rightLoader'>
-            <CircularProgress />
-            </div>
-          }
-          <div id="auth-container">
-          </div>
-        </div>
-
-      </div>
-      </>
+        </>
       }
       {showToast.show ? <Snackbar
-        open={showToast.show }
+        open={showToast.show}
         autoHideDuration={2000}
-        message={showToast.message }
-      />: null}
+        message={showToast.message}
+      /> : null}
     </div>
   );
 }
