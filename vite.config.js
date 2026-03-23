26274d864c58bcba285075a76a2255230c6a5345
NamrathaNP <namratha.petkar@impelsys.com>
2026-03-23 14:55:58 +0530

    sourcemap

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/SampleReact/"
+  base: "/SampleReact/",
+   build: {
+    sourcemap: true, // Set to false to disable .map files in production
+  },
 })
